43171737eb8ea2f8420460e1a8349d1bbd7b6540
Tom Novacek <tomas.novacek@eccam.com>
2022-03-13 23:18:42 +0100

    remove pricing h2

--- src/templates/pricing-page.js
+++ src/templates/pricing-page.js
@@ -19,9 +19,9 @@ export const PricingPageTemplate = ({ image, title, content, contentComponent })
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+{/*               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
-              </h2>
+              </h2> */}
               <PageContent className="content" content={content} />
             </div>
           </div>
