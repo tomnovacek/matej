2208b6f3af9a2bf4eeab2db61f8dc06e711243d8
Tom Novacek <tomas.novacek@eccam.com>
2022-03-17 15:44:16 +0100

    Add Font Awesome Icons.

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -5,6 +5,10 @@ import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 import FullWidthImage from "../components/FullWidthImage";
 import { getImage } from "gatsby-plugin-image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLocationDot, faCircleInfo, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+//import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'; // <-- import styles to be used
+
 
 // eslint-disable-next-line
 export const ContactPageTemplate = ({ image, title, content, contentComponent }) => {
@@ -28,18 +32,13 @@ export const ContactPageTemplate = ({ image, title, content, contentComponent })
                           <a href="https://www.google.com/maps/place/Fra%C5%88a+Mojtu+1,+949+01+Nitra,+Slovakia/@48.3119326,18.0890001,17z/data=!3m1!4b1!4m5!3m4!1s0x476b3ee0d61f50e3:0x20c50d66bb142a2!8m2!3d48.3119326!4d18.0911888" target="_blank" rel="noreferrer"><img src="/img/map.png" alt="Map" /></a>
                       </div>
                       <div className="column">
-                          <dl>
-                              <dt>Meno</dt>
-                              <dd>Mgr. Matej Borovský</dd>
-                              <dt>Adresa</dt>
-                              <dd>Fraňa Mojtu 1, Nitra, 949 01</dd>
-                              <dt>Info</dt>
-                              <dd>Termíny sedení na základe dohody</dd>
-                              <dt>Telefon</dt>
-                              <dd>+421 914 175 148 </dd>
-                              <dt>Email</dt>
-                              <dd>psychologborovsky@gmail.com</dd>
-                          </dl>
+                          <ul className="no-bullets">
+                              <li><FontAwesomeIcon icon={faUser}/> Mgr. Matej Borovský</li>
+                              <li><FontAwesomeIcon icon={faLocationDot}/> Fraňa Mojtu 1, Nitra, 949 01</li>
+                              <li><FontAwesomeIcon icon={faCircleInfo}/> Termíny sedení na základe dohody</li>
+                              <li><FontAwesomeIcon icon={faPhone}/> +421 914 175 148 </li>
+                              <li><FontAwesomeIcon icon={faEnvelope}/> psychologborovsky@gmail.com</li>
+                          </ul>
                       </div>
                   </div>
               </div>
