3f9c4a73de834645ef4ab11d2cbbd1bc97765ca7
Tom Novacek <tom.novacek@gmail.com>
2022-03-22 21:13:58 +0100

    cms config about page

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -5,6 +5,7 @@ import { AboutPageTemplate } from '../../templates/about-page'
 const AboutPagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
     title={entry.getIn(['data', 'title'])}
+    introduction={entry.getIn(['data', 'introduction'])}
     content={widgetFor('body')}
   />
 )
