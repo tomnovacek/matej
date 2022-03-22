4fe2f112fd17bc3a9461c451b0589e1adaee43e8
Tom Novacek <tom.novacek@gmail.com>
2022-03-22 23:23:22 +0100

    fix cms previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import IndexPagePreview from './preview-templates/ContactPagePreview'
 import PricingPagePreview from './preview-templates/PricingPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
 
